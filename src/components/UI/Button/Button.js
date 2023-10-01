7044ee7b11a8453819ab13e6630607ca8f1d1601
tah01 <tahirkadgaonkar01@gmail.com>
2023-10-01 10:52:34 +0530

    using CSS module for styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,37 +1,38 @@
-//external pakge for styling component without affecting otehr components - even if they contain same class name
-//lot of features explained in DOCUMENTATAION
-import styled from "styled-components";
+// //external pakge for styling component without affecting otehr components - even if they contain same class name
+// //lot of features explained in DOCUMENTATAION
+// import styled from "styled-components";
 
-const Button = styled.button`
-  font: inherit;
-  width: 100%;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   font: inherit;
+//   width: 100%;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
-  &:focus {
-    outline: none;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+import styles from "./Button.module.css";
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
