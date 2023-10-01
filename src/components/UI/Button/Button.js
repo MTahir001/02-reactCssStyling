6c04ba3f8a3479623cd03477fbc2c778c00d6738
tah01 <tahirkadgaonkar01@gmail.com>
2023-10-01 09:50:21 +0530

    installed: styled-component replaced external styling of Button compo with styled-compo

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,33 @@
-import React from "react";
+//external pakge for styling component without affecting otehr components - even if they contain same class name
+//lot of features explained in DOCUMENTATAION
+import styled from "styled-components";
 
-import "./Button.css";
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
